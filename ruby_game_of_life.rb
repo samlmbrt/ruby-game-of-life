c2da8f3755c4726d2c655ef59ed3e2013e2a70c5
Samuel Lambert <samuel.lambert@hotmail.com>
2022-09-25 18:43:51 -0400

    Allow specifying size of a cell on the commandline

--- ruby_game_of_life.rb
+++ ruby_game_of_life.rb
@@ -6,31 +6,30 @@ require 'slop'
 # Game of Life window
 class Window < Gosu::Window
   def initialize(rows:, columns:)
-    super rows, columns
+    super(rows, columns)
     self.caption = 'Game of Life'
   end
 
-  def update
-  end
-
   def draw
+    # draw_rect(@x, 10, 5, 5, Gosu::Color.argb(0xff_808080))
   end
 end
 
 def parse_arguments
   Slop.parse(suppress_errors: true) do |o|
-    o.int('-r', '--rows', 'Number of rows', default: 800)
-    o.int('-c', '--columns', 'Number of columns', default: 800)
+    o.int('-r', '--rows', 'Number of rows', default: 100)
+    o.int('-c', '--columns', 'Number of columns', default: 100)
+    o.int('-s', '--size', 'Cell size in pixels', default: 5)
     o.separator('')
     o.separator('other options:')
     o.on('-h', '--help', 'Show this message') do
-      puts o
+      puts(o)
       exit
     end
   end
 end
 
 options = parse_arguments
-rows, columns = options.to_hash.values_at(:rows, :columns)
-window = Window.new(rows: rows, columns: columns)
+rows, columns, size = options.to_hash.values_at(:rows, :columns, :size)
+window = Window.new(rows: rows * size, columns: columns * size)
 window.show
